5aaa7cf917ee9d1312481094d2ebe2e9af481a1e
HuangYaobin <178746489@qq.com>
2024-01-13 21:20:14 +0800

    feat: 利用dom api简单实现显示文本

--- mian.js
+++ mian.js
@@ -1,1 +1,10 @@
-console.log('x')
+const root = document.getElementById('root')
+
+const appNode = document.createElement('div')
+appNode.setAttribute('id', 'app')
+
+const textNode = document.createTextNode('hi-mini-react')
+
+appNode.appendChild(textNode)
+
+root.appendChild(appNode)
