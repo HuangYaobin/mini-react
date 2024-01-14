a7cdd82e01c366914f10d879adf051973685a372
HuangYaobin <178746489@qq.com>
2024-01-14 23:59:34 +0800

    feat: 实现fiber架构

--- core/React.js
+++ core/React.js
@@ -1,18 +1,82 @@
 export function render (el, container) {
-  const dom = el.type === 'TEXT_ELEMENT'
-    ? document.createTextNode(el.props.nodeValue)
-    : document.createElement(el.type)
+  workLoop(el, container)
+}
 
-  Object.entries(el.props).forEach(([key, value]) => {
-    if (key !== 'children') {
-      dom[key] = value
+function workLoop (el, container) {
+  let nextWorkOfUnit = {
+    props: {
+      children: [el]
+    },
+    dom: container,
+    child: null,
+    sibling: null,
+    parent: null,
+  }
+
+  requestIdleCallback(
+    function work (IdleDeadline) {
+      let shouldYield = IdleDeadline.timeRemaining() < 1
+
+      while (!shouldYield && nextWorkOfUnit) {
+        nextWorkOfUnit = performWorkOfUnit(nextWorkOfUnit)
+
+        shouldYield = IdleDeadline.timeRemaining() < 1
+      }
+
+      requestIdleCallback(work)
     }
+  )
+}
+
+function performWorkOfUnit (fiber) {
+  if (!fiber.dom) {
+    const dom = (
+      fiber.dom = fiber.type === 'TEXT_ELEMENT'
+        ? document.createTextNode(fiber.props.nodeValue)
+        : document.createElement(fiber.type)
+    )
+
+    Object.entries(fiber.props).forEach(([key, value]) => {
+      if (key !== 'children') {
+        dom[key] = value
+      }
+    })
+
+    fiber.parent?.dom.appendChild(dom)
+  }
+
+  let preFiber = null
+  fiber.props.children.forEach((child, index) => {
+    const newFiber = {
+      type: child.type,
+      props: child.props,
+      dom: null,
+      child: null,
+      sibling: null,
+      parent: fiber,
+    }
+
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      preFiber.sibling = newFiber
+    }
+
+    preFiber = newFiber
   })
 
-  el.props.children.forEach(el => render(el, dom))
+  if (fiber.child) {
+    return fiber.child
+  }
 
-  container.appendChild(dom)
+  if (fiber.sibling) {
+    return fiber.sibling
+  }
+
+  // 要递归找叔叔?
+  return fiber.parent?.sibling
 }
+
 
 export function createElement (type, props, ...children) {
   return {
@@ -43,3 +107,5 @@ const React = {
 }
 
 export default React
+
+
